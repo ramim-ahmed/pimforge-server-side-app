0ee64572f173c96670ef959960b10d2bb4c689a5
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-05-10 14:16:55 +0600

    [DONE] querie-service-with-database

--- src/app/modules/querie/querie.controller.js
+++ src/app/modules/querie/querie.controller.js
@@ -19,6 +19,61 @@ const createNew = async (req, res) => {
   }
 };
 
+const getAllQueries = async (req, res) => {
+  try {
+    const result = await querieService.getAllQueries();
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "Queries Fetch All Successfully!!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json({
+      success: true,
+      message: "Queries Fetch All Failed!!",
+      error,
+    });
+  }
+};
+const getMyAllQueries = async (req, res) => {
+  try {
+    const { email } = req.query;
+    const result = await querieService.getMyAllQueries(email);
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "My Queries Fetch All Successfully!!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json({
+      success: true,
+      message: "My Queries Fetch All Failed!!",
+      error,
+    });
+  }
+};
+
+const getSingleQuerie = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const result = await querieService.getSingleQuerie(id);
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "Querie Fetch Successfully!!",
+      data: result,
+    });
+  } catch (error) {
+    res.status(httpStatus.BAD_REQUEST).json({
+      success: true,
+      message: "Querie Fetch Failed!!",
+      error,
+    });
+  }
+};
+
 module.exports.querieController = {
   createNew,
+  getAllQueries,
+  getMyAllQueries,
+  getSingleQuerie,
 };
--- src/app/modules/querie/querie.service.js
+++ src/app/modules/querie/querie.service.js
@@ -5,6 +5,38 @@ const createNew = async (data) => {
   return result;
 };
 
+const getAllQueries = async () => {
+  const result = await Querie.find({}).sort({ createdAt: "desc" });
+  return result;
+};
+
+const getMyAllQueries = async (email) => {
+  const result = await Querie.find({ "user.email": email }).sort({
+    createdAt: "desc",
+  });
+  return result;
+};
+
+const getSingleQuerie = async (id) => {
+  const result = await Querie.findOne({ _id: id });
+  return result;
+};
+
+const updateQuerie = async (id, data) => {
+  const result = await Querie.updateOne({ _id: id }, data);
+  return result;
+};
+
+const deleteQuerie = async (id) => {
+  const result = await Querie.deleteOne({ _id: id });
+  return result;
+};
+
 module.exports.querieService = {
   createNew,
+  getAllQueries,
+  getMyAllQueries,
+  getSingleQuerie,
+  updateQuerie,
+  deleteQuerie,
 };
