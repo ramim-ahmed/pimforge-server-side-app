f2be141282466dd7110e2c6add3c0352b55dd4e5
ramim-ahmed <ramimahmedraj2@gmail.com>
2024-05-10 14:35:30 +0600

    [DONE] querie-api-to-manage-querie-data

--- src/app/modules/querie/querie.route.js
+++ src/app/modules/querie/querie.route.js
@@ -2,6 +2,11 @@ const express = require("express");
 const { querieController } = require("./querie.controller");
 const router = express.Router();
 
+router.get("/", querieController.getAllQueries);
+router.get("/my-queries", querieController.getMyAllQueries);
+router.get("/:id", querieController.getSingleQuerie);
 router.post("/create-new", querieController.createNew);
+router.patch("/:id", querieController.updateQuerie);
+router.delete("/:id", querieController.deleteQuerie);
 
 module.exports.querieRoutes = router;
